a63f9defe7c2ba3c59e43a50a7ea9b4a3ef22a0e
utkristha <utkristhabhattarai@gmail.com>
2025-05-15 21:53:17 +0545

    Login page changed to display utkristha.

--- Project/src/components/Login/Login.jsx
+++ Project/src/components/Login/Login.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import "./Login.css";
 export default function Login() {
   return (
-    <div id="key">Login page done by Roshan.</div>
+    <div id="key">Login page done by Utkristha.</div>
   )
 }
