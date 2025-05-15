d427c07daf349acddb8c88d8c4cc75e92d075935
Rachit Khadka <rkchhetri358@gmail.com>
2025-05-15 22:02:34 +0545

    Roshan branch is deleted

--- Project/src/components/Login/Login.jsx
+++ Project/src/components/Login/Login.jsx
@@ -2,10 +2,8 @@ import React from 'react'
 import "./Login.css";
 export default function Login() {
   return (
-<<<<<<< HEAD
     <div id="key">Login page done by utk.</div>
-=======
     <div id="key">Login page done by rachit.</div>
->>>>>>> 77c5e394c21d8153221186d56aa2ac85c3a0d8ad
+    
   )
 }
