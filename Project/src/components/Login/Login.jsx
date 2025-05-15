cf009bf4439923c0ed2328658373140c07dfc44b
Rachit Khadka <rkchhetri358@gmail.com>
2025-05-15 22:40:17 +0545

    Check

--- Project/src/components/Login/Login.jsx
+++ Project/src/components/Login/Login.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import "./Login.css";
 export default function Login() {
   return (
-<<<<<<< HEAD
     <div id="key">Login page done by Roshan.</div>
     <div id="key">Login page done by virat.</div>
     <div id="key">Login page done by kphli.</div>
-=======
     <div id="key">Login page done by utk.</div>
     <div id="key">Login page done by rachit.</div>
     
->>>>>>> e584caca25e52feeee8986cca0556a15dbcc037b
   )
 }
