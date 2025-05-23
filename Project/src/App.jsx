c59867ba7076a914dd054b1ace04dcafc9acf76a
Rachit Khadka <rkchhetri358@gmail.com>
2025-05-23 09:30:11 +0545

    unused

--- Project/src/App.jsx
+++ Project/src/App.jsx
@@ -5,6 +5,7 @@ import Signup from "./components/Signup/Signup";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from './components/Home/Home';
+import Complaint from './components/Complaint/Complaint';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/complaint" element={<Complaint/>} />
         </Routes>
       <footer>
  <Footer />
--- Project/src/components/Complaint/Complaint.jsx
+++ Project/src/components/Complaint/Complaint.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import "./Complaint.css";
+import { NavLink, useNavigate } from "react-router-dom";
+
+
+
 
 export default function Complaint() {
   return (
     <>
-        
+      <div className="sidebar">
+      {/* <img src={logo} alt="Logo" style={{ marginBottom: "20px" }} /> */}
+      <ul>
+        <li>
+          <NavLink to="transaction" className={({ isActive }) => (isActive ? "active" : "")}>
+            Transactions
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="insertProducts" className={({ isActive }) => (isActive ? "active" : "")}>
+            Insert Products
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="sales" className={({ isActive }) => (isActive ? "active" : "")}>
+            Sales Details
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="available_items" className={({ isActive }) => (isActive ? "active" : "")}>
+            Available Items
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="graph" className={({ isActive }) => (isActive ? "active" : "")}>
+            Graph
+          </NavLink>
+        </li>
+      </ul>
+      
+      <div className="button">
+
+      <button className="logout-btn" >Logout</button>
+      </div>
+    </div>
 
     </>
   )
